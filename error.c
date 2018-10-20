4dcf2de1ba277caec0a3ff3c32bdac6b9c9805e4
thk2b <theokobb@gmail.com>
2018-10-19 20:00:42 -0700

    fix spacing

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/19 19:39:02 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/19 19:41:41 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/19 20:00:06 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		error(const char *msg, const char *cmd, int ret)
 	if (cmd)
 	{
 		ft_putstr(cmd);
-		ft_putchar(':');
+		ft_putstr(": ");
 	}
 	ft_putendl(msg);
 	return (ret);
