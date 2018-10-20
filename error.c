b51c7edade942c2d1d3386b618ec474be388e62a
thk2b <theokobb@gmail.com>
2018-10-19 20:14:58 -0700

    refactor error

--- error.c
+++ error.c
@@ -6,21 +6,22 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/19 19:39:02 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/19 20:00:06 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/19 20:14:30 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 
-int		error(const char *msg, const char *cmd, int ret)
+int		error(const char *predicate, const char *subject, int ret)
 {
 	ft_putstr("minishell: ");
-	if (cmd)
+	ft_putstr(predicate);
+	if (subject)
 	{
-		ft_putstr(cmd);
 		ft_putstr(": ");
+		ft_putstr(subject);
 	}
-	ft_putendl(msg);
+	ft_putendl("");
 	return (ret);
 }
