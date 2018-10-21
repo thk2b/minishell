9e33d64767d9d8a81121b5b3f60aa39007059b07
thk2b <theokobb@gmail.com>
2018-10-20 20:50:48 -0700

    rm printf

--- builtin_env.c
+++ builtin_env.c
@@ -6,21 +6,19 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/20 20:42:56 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/20 20:47:09 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/20 20:50:42 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 #include "libft.h"
 #include <stdlib.h>
-#include <printf.h>
 
 int	builtin_env(const char **av, const char **env)
 {
 	size_t	i;
 
 	(void)av;
-	printf("A\n");
 	i = 0;
 	while (env[i])
 		ft_putendl(env[i++]);
