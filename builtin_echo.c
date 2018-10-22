fafbb5fdc74a2aecddf3d6e36d428bc8a5aef9ca
thk2b <theokobb@gmail.com>
2018-10-21 17:25:01 -0700

    fix echo

--- builtin_echo.c
+++ builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/20 16:46:59 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/21 15:23:57 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/21 17:24:12 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,16 @@ int		builtin_echo(const char **argv)
 	int		i;
 	ssize_t	status;
 
-	i = 0;
+	i = 1;
 	status = 0;
 	while (argv[i])
 	{
 		status = write(1, argv[i], ft_strlen(argv[i]));
-		status += write(1, "\n", 1);
+		status += write(1, " ", 1);
 		if (status <= 0)
 			return(1);
+		i++;
 	}
+	status += write(1, "\n", 1);
 	return (0);
 }
