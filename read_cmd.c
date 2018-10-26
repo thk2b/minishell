fb609184fecde6dff07827d93dbadda2376417d8
thk2b <theokobb@gmail.com>
2018-10-25 23:30:37 -0700

    rewrite

--- read_cmd.c
+++ read_cmd.c
@@ -6,23 +6,30 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/25 18:55:20 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/25 19:04:05 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/25 23:26:54 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "get_next_line.h"
 #include "libft.h"
+#include <unistd.h>
 
-int	read_cmd(t_runtime *rt)
+char	**read_cmd(void)
 {
 	char	*line;
 	char	**cmd;
 
 	if (get_next_line(0, &line) != 1)
-		return (error("cannot read", NULL, 1));
-	if((cmd = ft_strsplit(line, ' ')) == NULL)
-		return (error("cannot split", NULL, 1));
-	rt->cmd = cmd;
-	return (0);
+	{
+		error("cannot read", NULL, 0);
+		return (NULL);
+	}
+	if((cmd = ft_strsplit_escape(line, ' ', "\"\'")) == NULL)
+	{
+		error("cannot split", NULL, 0);
+		return (NULL);
+	}
+	free(line);
+	return (cmd);
 }
