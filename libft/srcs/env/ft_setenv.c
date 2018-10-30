4dcbf4343845e7a8175fcaa9be868d71f8359950
thk2b <theokobb@gmail.com>
2018-10-29 22:27:33 -0700

    improve setenv

--- libft/srcs/env/ft_setenv.c
+++ libft/srcs/env/ft_setenv.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 23:23:59 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/27 23:32:37 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/29 22:25:23 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,35 @@
 #include "libft.h"
 #include <unistd.h>
 
+char		**ft_getenvp(const char *name)
+{
+	extern char **environ;
+	int			i;
+	char		*end;
+
+	i = 0;
+	while (environ[i])
+	{
+		if ((end = ft_strchr(environ[i], '=')) == NULL)
+			;
+		else if (ft_strncmp(environ[i], name, end - environ[i]) == 0)
+			return (environ + i);
+		i++;
+	}
+	return (NULL);
+}
+
 int		ft_setenv(const char *name, const char *value, int overwrite)
 {
-	char	*current;
+	char	**current;
 	char	*str;
 
-	current = ft_getenv(name);
+	current = ft_getenvp(name);
 	if (current && overwrite == 0)
 		return (-1);
-	ft_unsetenv(name);
 	MCK(str = ft_strcjoin(name, '=', value), -1);
-	return (ft_putenv(str));
+	if (current == NULL)
+		return (ft_putenv(str));
+	free(*current);
+	return ((*current = str) != 0);
 }
