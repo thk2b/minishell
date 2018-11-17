759348480d89af52ad1ec397d947da833701ec10
thk2b <theokobb@gmail.com>
2018-11-16 20:05:52 -0800

    patch unsetenv

--- libft/srcs/env/ft_unsetenv.c
+++ libft/srcs/env/ft_unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 23:33:15 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/28 00:01:17 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/11/16 20:04:32 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,15 @@ static int	get_env_index(const char *name)
 	extern char	**environ;
 	int			i;
 	char		*end;
+	int			len;
 
+	len = ft_strlen(name);
 	i = 0;
 	while (environ[i])
 	{
 		if ((end = ft_strchr(environ[i], '=')) == NULL)
+			;
+		if (end - environ[i] != len)
 			;
 		else if (ft_strncmp(environ[i], name, end - environ[i]) == 0)
 			return (i);
